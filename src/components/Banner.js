296118a133d305ad32380367c816917a0e0c3766
Wesleson22 <wesllesonsowza@gmail.com>
2023-05-29 18:20:02 -0300

    Portifolio versão

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Dev Frontend", "Dev Backend", "Dev Mobile", "UI/UX Designer" ];
   const period = 2000;
 
   useEffect(() => {
