bcd1d57d7b3346ead619e680893b083b7de47363
Wesleson22 <wesllesonsowza@gmail.com>
2023-05-29 19:12:53 -0300

    Portifolio versão 5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
+import navIcon from '../assets/img/nav-icon.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -50,9 +51,10 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/weslleson22"><img src={navIcon} alt="" /></a>
+                <a href="https://www.linkedin.com/in/wesleson-souza-silva-a598b8152/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/weslleson.santos/"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/eng_weslleson9/?igshid=MzNlNGNkZWQ4Mg%3D%3D"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
