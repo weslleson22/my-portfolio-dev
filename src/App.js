e54b75c7424e8dbf49ed6b31305d295ceb4d50f7
Wesleson22 <wesllesonsowza@gmail.com>
2023-05-29 17:35:31 -0300

    Portifolio versão 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/NavBar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-    </div>  
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
+
 export default App;
